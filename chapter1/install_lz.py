1258ba6d5a7343e970f540a3f4deb63bd41a9f2c
luozhengszj <lz3553772>
2019-03-19 23:02:41 +0800

    安装opencv、numpy、git使用；图像的输入输出转置、numpy的基本使用等

---
+++ chapter1/install_lz.py
@@ -0,0 +1,52 @@
+# 进入..\opencv文件夹，创建虚拟环境：virtualenv venv -p python3
+# 打开cmd，进入虚拟环境..\opencv\env\Scripts
+# activite 进入虚拟环境
+# 下载numpy: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy
+# 下载opencv-python: pip install -i https://pypi.tuna.tsinghua.edu.cn/simple opencv-python
+
+# 测试
+import cv2 as cv
+import numpy as np
+
+print(cv.__version__)
+print(np.__version__)
+
+'''
+4.0.0
+1.16.2
+'''
+
+'''
+备注：
+若在pip升级的时候：python -m pip install --upgrade pip
+出现错误：AttributeError: 'NoneType' object has no attribute 'bytes'
+解决方案：easy_install -U pip
+'''
+
+'''
+github提交文件：
+1.在github新建仓库
+2.打开Git Bash ，进入../opencv
+3.依次执行一下命令：
+echo "# OpenCV3-note" >> README.md
+git init
+git add .
+git commit -m "Install opencv numpy"
+git remote add origin https://github.com/luozhengszj/OpenCV3-note.git
+git config --global credential.helper store
+git push -u origin master
+4.完成推送，其中git config --global credential.helper store是为了避免下次还需要输入用户密码
+
+以后每次推送命令如下：
+git add .
+--git status
+git commit -m "备注"
+git log
+--git rebase -i b0aa963
+--改pick 为 drop，保存退出
+git push -u origin master
+
+5.若出错：error: failed to push some refs to
+先拉取到本地仓库（会自动同步合并）git pull origin master
+再使用 git push -u origin master 推送
+'''
---
+++ chapter2/opencv_io_numpy.py
@@ -0,0 +1,106 @@
+"""
+第2章 处理文件、摄像头和图形用户界面
+"""
+
+import numpy as np
+import cv2 as cv
+
+img_path = '.\static\cat.png'
+
+
+# 获取图像具体信息
+def get_image_info(image):
+    print(type(image))
+    print(image.shape)
+    print(image.size)  # size = 三通道相乘 蓝绿红 blue,gre
+    print(image.dtype)
+    pixel_data = np.array(image)
+    print(pixel_data)
+
+
+# numpy
+def access_pixels(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+    print('width: %s, height: %s, channels: %s'%(width, height, channels))
+    """
+    # 利用numpy倒置图片
+    dst = cv.bitwise_not(image)
+    cv.imshow('pixels image', dst)
+    """
+    # 循环倒置图片 慢！
+    for row in range(height):
+        for col in range(width):
+            for c in range(channels):
+                pv = image[row, col, c]
+                image[row, col, c] = 255 - pv
+
+    cv.imshow('pixels image', image)
+
+
+# 利用numpy创建图片
+def create_image():
+    """
+    img = np.zeros([400, 400, 3], np.uint8)
+    img[:, :, 1] = np.ones([400, 400]) * 255
+    """
+
+    """
+    img = np.zeros([400, 400, 1], np.uint8)
+    img[:, :, 0] = np.ones([400, 400]) * 127
+    """
+
+    """
+    img = np.ones([400, 400, 1], np.uint8)
+    img = img * 127  # 和上面效果一样
+    """
+
+    # cv.imshow('create image', img)
+
+    # img = np.ones([3, 3], np.float32)  # 类型影响截断！！会影响大小
+    img = np.ones([3, 3], np.uint8)
+    img.fill(12227.8888)
+
+    img2 = img.reshape([1, 9])
+    print(img)
+    print(img2)
+
+
+# 获取摄像头视频
+def video_demo():
+    capture = cv.VideoCapture(0)  # 写入视频路径或第几个摄像头
+    while 1:
+        ret, frame = capture.read()
+        cv.flip(frame, 1)  # 把视频取正
+        cv.imshow('video', frame)
+        c = cv.waitKey(50)
+        if c == 27:
+            break
+
+
+def main():
+    src = cv.imread(img_path)
+    cv.namedWindow('cat img', cv.WINDOW_AUTOSIZE)
+    cv.imshow('cat img', src)
+
+    # get_image_info(src)
+    # gray = cv.cvtColor(src,cv.COLOR_BGR2GRAY) #灰度图像
+    # cv.imwrite('.\static\cat_copy.png', gray) #保存图像
+    # video_demo() #读取摄像头视频
+
+    begin_time = cv.getTickCount()
+
+    access_pixels(src)
+    # create_image()
+
+    end_time = cv.getTickCount()
+    print('run time (s): %s' % ((end_time - begin_time)/cv.getTickFrequency()))
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
